0a65524d93240b7e0d8f68168a1316cd9342f9cd
Lara Converso <laraconverso@gmail.com>
2022-04-05 22:02:36 -0300

    update

---
+++ fetch-api-main/js/index.js
@@ -0,0 +1,40 @@
+let tarjeta = document.querySelector('.tarjeta');
+const btn = document.querySelector('.btnContainer');
+
+// Aquí realizamos un la consulta de la promesa, esperando su respuesta asíncrona
+function peticion(){
+   fetch('https://randomuser.me/api/')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        //manipulamos la respuesta
+        console.log(data.results[0]);
+        renderizarDatosUsuario(data.results[0]);
+        
+    }); 
+} 
+
+
+
+function renderizarDatosUsuario(datos) {
+    /* -------------------------------- CONSIGNA 1 -------------------------------- */
+    // Aquí deben desarrollar una función que muestre en pantalla:
+    // la foto, el nombre completo del usuario y su email.
+    // Esto debe estar basado en la info que nos llega desde la API e insertarse en el HTML.
+    
+    
+    tarjeta.innerHTML = `<p>${datos.name.first}</p></br>
+        <p>${datos.email}</p>
+        <img src=${datos.picture.medium} alt=${datos.name.first}>
+    `;
+}
+
+
+/* --------------------------- CONSIGNA 2 (extra) --------------------------- */
+// Aqui pueden ir por el punto extra de utilizar el boton que se encuentra comentado en el HTML
+// Pueden descomentar el código del index.html y usar ese boton para ejecutar un nuevo pedido a la API, sin necesidad de recargar la página.
+// Es criterio del equipo QUÉ bloque del código debe contenerse dentro de una función para poder ser ejecutada cada vez que se escuche un click.
+btn.addEventListener('click', function() {
+    peticion();
+})
